0fed3d7720b144fb77f31c5a1934a5fe2995759f
a.turki <at.aslem@gmail.com>
2023-11-14 11:14:51 +0100

    VO init project.

---
+++ src/main/java/com/proto/microServicePrototypoAuthentication/MicroServicePrototypoAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.proto.microServicePrototypoAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicePrototypoAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicePrototypoAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proto/microServicePrototypoAuthentication/MicroServicePrototypoAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proto.microServicePrototypoAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicePrototypoAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
